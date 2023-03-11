9ffdf29d3ab6db2c0febad1ce5fa7d809a5a9eeb
Rahul Sharma <developerrahul671@gmail.com>
2023-03-12 01:41:32 +0530

    Add files via upload

--- PatternPractice.java
+++ PatternPractice.java
@@ -1,3 +1,4 @@
+
 public class PatternPractice {
 
      static void pattern1(int number){
@@ -56,7 +57,7 @@ public class PatternPractice {
             }
             System.out.println();
         }
-    
+
 
         // WE CAN DO ONE MORE THING TO SOLVE THIS PATTERN
 
@@ -68,10 +69,50 @@ public class PatternPractice {
          }
     }
 
+    static void pattern4(int number){
+         /*
+         1
+         12
+         123
+         1234
+         12345
+          */
+         int count = 1;
+         for(int row = 1; row <=number; row++){
+             for (int col = 1; col <= row; col++){
+                 System.out.print(col + " ");
+             }
+             System.out.println();
+
+         }
+    }
+
+    static void pattern5(int number) {
+
+         /*
+         *
+         * *
+         * * *
+         * * * *
+         * * *
+         * *
+         *
+          */
+        for (int row = 1; row < 2 * number; row++) {
+          int totalCol = (row > number) ? 2 * number - row : row;
+          for (int j = 1 ; j <= totalCol; j++ ){
+              System.out.print("* ");
+           }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
     //    pattern1(5);
     //    pattern2(8);
-          pattern3(5);
+    //    pattern3(5);
+    //    pattern4(5);
+    //    pattern5(4);
     }
 
 }
